428fb54178766b9f311e65ad88b01938fda8b0ee
Mo7a7b <mohabibrahim181@gmail.com>
2026-03-13 06:57:33 +0000

    modified:   app/favicon.ico 	modified:   app/globals.css 	modified:   app/layout.tsx 	deleted:    app/page.tsx 	modified:   next.config.ts 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/file.svg 	deleted:    public/globe.svg 	deleted:    public/next.svg 	deleted:    public/vercel.svg 	deleted:    public/window.svg 	modified:   tsconfig.json 	app/(site)/ 	app/dashboard/ 	app/loading.tsx 	components.json 	components/ 	contexts/ 	hooks/ 	lib/ 	proxy.ts 	public/ 	types/

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        hostname: "img-c.udemycdn.com",
+      },
+    ],
+    qualities: [100, 75],
+  },
 };
 
 export default nextConfig;
